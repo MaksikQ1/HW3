34b61a5279bd25eebdf573c88c4d45bcb3a90cba
Maksim Bogolyubov <bogolyubovmaks@gmail.com>
2025-03-15 15:37:04 +0500

    added sources and changed TextBoxTest

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -12,11 +12,10 @@ import static com.codeborne.selenide.Selenide.open;
 public class TextBoxTests {
 
     @BeforeAll
-    static void beforeAll() {
+    static void setupConfig() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000; // default 4000
     }
 
